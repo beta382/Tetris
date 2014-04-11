beta382/Tetris

commit 5325cd6ccd2c3479bf723bdf63c2ce49788a0d28

Implimented tetromino ID's, proper special blocks, made some adjustments, removed unnecissary allocations, made ExplodingBlock example more awesome, etc.

src/GhostBlock.cpp
@@ -34,27 +34,83 @@ GhostBlock::~GhostBlock() {
 void GhostBlock::doOnClear(vector<vector<Block *> >& blockField, int x, int y) {
     // Do nothing
     
-    // ExplodingBlock example
-    /*for (int i = x-1; i <= x+1; i++) {
-        for (int j = y-1; j <= y+1; j++) {
-            if (i >= 0 && i < blockField.getSize() && j >= 0 && j < blockField[i].getSize() && blockField[i][j]) {
-                delete blockField[i][j];
+    // ExplodingBlock example, needs <ctime> and "Rectangle.h"
+    /*
+    // Make a big explosion block
+    
+    int explosionX = (x >= 2) ? getLocationX()-getTotalSize()*2 : getLocationX()-getTotalSize()*x;
+    
+    int explosionY = (y >= 2) ? getLocationY()-getTotalSize()*2 : getLocationY()-getTotalSize()*y;
+    
+    int explosionWidth;
+    
+    if (x >= 2 && (blockField.size()-x-1) >= 2) {
+        explosionWidth = getTotalSize()*5-getPadding();
+    } else if (x >= 2) {
+        explosionWidth = getTotalSize()*(blockField.size()-x+2)-getPadding();
+    } else {
+        explosionWidth = getTotalSize()*(x+3)-getPadding();
+    }
+
+    int explosionHeight;
+    if (y >= 2 && (blockField[0].size()-y-1) >= 2) {
+        explosionHeight = getTotalSize()*5-getPadding();
+    } else if (y >= 2) {
+        explosionHeight = getTotalSize()*(blockField.size()-y+2)-getPadding();
+    } else {
+        explosionHeight = getTotalSize()*(y+3)-getPadding();
+    }
+    
+    MyRectangle explosion(explosionX, explosionY, explosionWidth, explosionHeight, Color::RED, getBackground());
+    
+    // Blink two times
+    for (int r = 0; r < 3; r++) {
+        explosion.draw();
+        
+        g->Draw(); // Force screen redraw
+
+        clock_t start = clock();
+        
+        while (clock() < start+150); // Wait 150ms
+        
+        explosion.erase();
+        
+        g->Draw(); // Force screen redraw
+
+        start = clock();
+        
+        while (clock() < start+150); // Wait 150ms
+    }
+    
+    // Actually clear the blocks
+    for (int i = x-2; i <= x+2; i++) {
+        for (int j = y-2; j <= y+2; j++) {
+            if (i >= 0 && i < (int)blockField.size() && j >= 0 && j < (int)blockField[i].size() && blockField[i][j]) {
+                blockField[i][j]->erase();
+                
+                Block *tmp = blockField[i][j];
                 blockField[i][j] = NULL;
+                
+                tmp->doOnClear(blockField, i, j);
+                delete tmp;
             }
         }
-    }*/
+    }
     
+    //*/
+
     // GravityBlock example, needs <ctime>
-    /*bool didFall = true;
+    /*
+    bool didFall = true;
     while (didFall) {
         didFall = false;
         
-        for (int i = 0; i < blockField.getSize(); i++) {
-            for (int j = 0; j < blockField[i].getSize(); j++) {
-                if (j-1 >= 0 && blockField[i][j] && !blockField[i][j-1]) {
-                    blockField[i][j-1] = blockField[i][j]->makeNewClone();
+        for (unsigned int i = 0; i < blockField.size(); i++) {
+            for (unsigned int j = 1; j < blockField[i].size(); j++) {
+                if (blockField[i][j] && !blockField[i][j-1]) {
+                    blockField[i][j]->erase();
+                    blockField[i][j-1] = blockField[i][j];
 
-                    delete blockField[i][j];
                     blockField[i][j] = NULL;
                     
                     blockField[i][j-1]->setLocation(blockField[i][j-1]->getLocationX(), 
@@ -66,18 +122,23 @@ void GhostBlock::doOnClear(vector<vector<Block *> >& blockField, int x, int y) {
             }
         }
         
-        g->Draw();
+        g->Draw(); // Force screen redraw
         
         if (didFall) {
             clock_t start = clock();
             
             while (clock() < start + 100);
         }
-    }*/
+    }
+    //*/
 }
 
 GhostBlock *GhostBlock::makeNewClone() {
-    return new GhostBlock(*this);
+    GhostBlock *tmp = new GhostBlock(*this);
+    
+    tmp->setUniqueID(getUniqueID());
+    
+    return tmp;
 }
 
 void GhostBlock::draw() {

src/PlayingField.cpp
@@ -291,6 +291,17 @@ void PlayingField::doLineClear() {
         
         // Blink three times
         for (int r = 0; r < 3; r++) {
+            
+            for (unsigned int i = 0; i < clearableLines.size(); i++) {
+                for (int j = 0; j < getWidth(); j++) {
+                    if (blocks[j][clearableLines[i]]) { // Not really necessary since existence is guaranteed, but w/e
+                        blocks[j][clearableLines[i]]->draw();
+                    }
+                }
+            }
+            
+            g->Draw(); // Force screen redraw
+            
             start = clock();
             
             while (clock() < start+150); // Wait 150ms
@@ -308,28 +319,9 @@ void PlayingField::doLineClear() {
             start = clock();
             
             while (clock() < start+150); // Wait 150ms
-            
-            for (unsigned int i = 0; i < clearableLines.size(); i++) {
-                for (int j = 0; j < getWidth(); j++) {
-                    if (blocks[j][clearableLines[i]]) { // Not really necessary since existence is guaranteed, but w/e
-                        blocks[j][clearableLines[i]]->draw();
-                    }
-                }
-            }
-            
-            g->Draw(); // Force screen redraw
         }
         
-        // Erase the lines for good
-        for (unsigned int i = 0; i < clearableLines.size(); i++) {
-            for (int j = 0; j < getWidth(); j++) {
-                if (blocks[j][clearableLines[i]]) { // Not really necessary since existence is guaranteed, but w/e
-                    blocks[j][clearableLines[i]]->erase();
-                }
-            }
-        }
-        
-        // Clone, store, and then clear the blocks
+        // Clone, store, and then remove the blocks
         for (unsigned int i = 0; i < clearableLines.size(); i++) {
             for (int j = 0; j < getWidth(); j++) {
                 if (blocks[j][clearableLines[i]]) { // Not really necessary since existence is guaranteed, but w/e
@@ -340,9 +332,41 @@ void PlayingField::doLineClear() {
             }
         }
         
-        // Perform each block's special effect and delete the copy, should probably find a way to convert the remainder
-        // of the special tetromino to a normal tetromino
-        // TODO: Above, maybe have some sort of unique id for each Tetromino's blocks
+        // Go through the list of cleared blocks and find all blocks from the same tetromino into standard blocks
+        for (unsigned int i = 0; i < clearedBlocks.size(); i++) {
+            
+            // If the block we are on has a "no-ID", we don't need to check
+            if (clearedBlocks[i]->getUniqueID() != 0) {
+                
+                // Go through each block on the field and check for ones with the same unique ID, and replace them
+                for (int j = 0; j < getWidth(); j++) {
+                    for (int k = 0; k < getHeight(); k++) {
+                        if (blocks[j][k] && blocks[j][k]->getUniqueID() != 0 &&
+                                blocks[j][k]->getUniqueID() == clearedBlocks[i]->getUniqueID())
+                        {
+                            Block *tmp = blocks[j][k];
+                            blocks[j][k] = new Block(*tmp); // Not making a clone, this will give us a base Block
+                            delete tmp;
+                            blocks[j][k]->draw();
+                        }
+                    }
+                }
+                
+                // Go through the list of cleared blocks, check for ones with the same unique ID, and replace them
+                for (unsigned int j = i+1; j < clearedBlocks.size(); j++) {
+                    if (clearedBlocks[j]->getUniqueID() != 0 &&
+                            clearedBlocks[i]->getUniqueID() == clearedBlocks[j]->getUniqueID())
+                    {
+                        Block *tmp = clearedBlocks[j];
+                        clearedBlocks[j] = new Block(*tmp); // Not making a clone, this will give us a base Block
+                        delete tmp;
+                        clearedBlocks[j]->draw();
+                    }
+                }
+            }
+        }
+        
+        // For each cleared block, perform the block's special effect
         for (unsigned int i = 0; i < clearedBlocks.size(); i++) {
             clearedBlocks[i]->doOnClear(blocks, 
                     (clearedBlocks[i]->getLocationX()-getLocationX())/clearedBlocks[i]->getTotalSize(),

src/Block.cpp
@@ -11,21 +11,25 @@
 
 Block::Block (): Drawable(0, 0, 10, 10) {
     padding = 0;
+    uniqueID = 0;
 }
 
 Block::Block (int x, int y, int size, int padding, unsigned int foreground, unsigned int background):
         Drawable(x, y, size, size, foreground, background) {
     this->padding = padding;
+    uniqueID = 0;
 }
 
 Block::Block (const Block& other): Drawable(other) {
     padding = other.padding;
+    uniqueID = 0;
 }
 
 Block& Block::operator =(const Block& rhs) {
     if (this != &rhs) {
         Drawable::operator =(rhs);
         padding = rhs.padding;
+        uniqueID = 0;
     }
     
     return *this;
@@ -47,12 +51,24 @@ int Block::getTotalSize() const {
     return getSize()+getPadding();
 }
 
+unsigned int Block::getUniqueID() const {
+    return uniqueID;
+}
+
+void Block::setUniqueID(unsigned int id) {
+    uniqueID = id;
+}
+
 void Block::doOnClear(vector<vector<Block *> >& blockField, int x, int y) {
     // Do nothing
 }
 
 Block *Block::makeNewClone() {
-    return new Block(*this);
+    Block *tmp = new Block(*this);
+    
+    tmp->setUniqueID(getUniqueID());
+    
+    return tmp;
 }
 
 /* ---------- Implemented from Drawable ---------- */

src/Block.h
@@ -63,6 +63,9 @@ class Block: public Drawable {
         int getPadding() const;
         int getTotalSize() const;
         
+        unsigned int getUniqueID() const;
+        void setUniqueID(unsigned int);
+        
         virtual void doOnClear(vector<vector<Block *> >&, int x, int y);
         virtual Block *makeNewClone();
         
@@ -71,6 +74,7 @@ class Block: public Drawable {
         void erase();
     protected:
         int padding;
+        unsigned int uniqueID;
 };
 
 #endif /* BLOCK_H_ */

src/Game.cpp
@@ -149,6 +149,7 @@ void Game::respondToKey(int key) {
             
             break;
         case 'e': // Rotate CW
+            // If we can rotate the current tetromino CW within the block field, do so
             if (field->canRotateCW(currentTetromino)) {
                 doRotateCW();
             } else { // If we can't, try doing a wall kick
@@ -233,6 +234,29 @@ void Game::respondToKey(int key) {
             shadow->draw();
             currentTetromino->draw();
             
+            break;
+        case 'g': // Spawn Ghost tetromino for testing
+            delete currentTetromino;
+            delete shadow;
+            
+            { // C++ throws a fit about variables in case statements, so this just sets a scope for `shape`
+                TetrominoShape shape = static_cast<TetrominoShape>(rand()%7); // Random TetrominoShape
+                
+                // Spawn a new tetromino and create a shadow in the same place
+                currentTetromino = field->spawnNewTetromino<GhostBlock>(shape);
+                shadow = new Tetromino<GhostBlock>(currentTetromino->getLocationX(), currentTetromino->getLocationY(),
+                        currentTetromino->getBlockSize(), currentTetromino->getPadding(), shape,
+                        field->getForeground());
+            }
+            
+            // Have the shadow fall
+            while (field->canShiftDown(shadow)) {
+                shadow->shiftDown();
+            }
+            
+            // Draw the new shadow then the new tetromino, so that the new tetromino may overlap the shadow
+            shadow->draw();
+            currentTetromino->draw();
             break;
         default:
             cout << key << endl;

src/Tetromino.h
@@ -208,6 +208,12 @@ void Tetromino<BlockType>::initTetromino (TetrominoShape type) {
             setHeight(2);
             break;
     }
+    
+    for (int i = 0; i < numBlocks(); i++) {
+        blocks[i]->setUniqueID(uniqueID);
+    }
+    
+    uniqueID++;
 }
 
 #endif /* TETROMINO_H_ */

src/TetrominoBase.cpp
@@ -9,6 +9,8 @@
 
 #include "TetrominoBase.h"
 
+unsigned int TetrominoBase::uniqueID = 1; // Reserve 0 as a "no-ID"
+
 TetrominoBase::TetrominoBase(): Shape() {
     offsetX = 0;
     offsetY = 0;

src/TetrominoBase.h
@@ -69,6 +69,8 @@ class TetrominoBase: public Shape {
         
         int offsetX;
         int offsetY;
+        
+        static unsigned int uniqueID;
 };
 
 

commit 4fb23452a374fe5bc86f14ff3096c417a1654d42

Fixed a small bug introduced in 5325cd6

src/GhostBlock.cpp
@@ -9,6 +9,11 @@
 
 #include "GhostBlock.h"
 
+/* Just put an extra slash in front to un-comment
+#include <ctime>
+#include "Rectangle.h"
+//*/
+
 GhostBlock::GhostBlock(): Block() {}
 
 GhostBlock::GhostBlock(int x, int y, int size, int padding, unsigned int foreground, unsigned int background):
@@ -35,7 +40,7 @@ void GhostBlock::doOnClear(vector<vector<Block *> >& blockField, int x, int y) {
     // Do nothing
     
     // ExplodingBlock example, needs <ctime> and "Rectangle.h"
-    /*
+    /* Just put an extra slash in front to un-comment
     // Make a big explosion block
     
     int explosionX = (x >= 2) ? getLocationX()-getTotalSize()*2 : getLocationX()-getTotalSize()*x;
@@ -100,7 +105,7 @@ void GhostBlock::doOnClear(vector<vector<Block *> >& blockField, int x, int y) {
     //*/
 
     // GravityBlock example, needs <ctime>
-    /*
+    /*// Just put an extra slash in front to un-comment
     bool didFall = true;
     while (didFall) {
         didFall = false;

src/PlayingField.cpp
@@ -347,7 +347,7 @@ void PlayingField::doLineClear() {
                             Block *tmp = blocks[j][k];
                             blocks[j][k] = new Block(*tmp); // Not making a clone, this will give us a base Block
                             delete tmp;
-                            blocks[j][k]->draw();
+                            blocks[j][k]->draw(); // Redraw the new block since what used to be there just got erased
                         }
                     }
                 }
@@ -359,8 +359,7 @@ void PlayingField::doLineClear() {
                     {
                         Block *tmp = clearedBlocks[j];
                         clearedBlocks[j] = new Block(*tmp); // Not making a clone, this will give us a base Block
-                        delete tmp;
-                        clearedBlocks[j]->draw();
+                        delete tmp; // Don't redraw, because these blocks aren't supposed to be visible.
                     }
                 }
             }