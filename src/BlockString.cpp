906f6641ce0544498961f22986a29814b0b4c460
beta382 <beta382@gmail.com>
2014-04-29 22:38:12 -0500

    Documentation

--- src/BlockString.cpp
+++ src/BlockString.cpp
@@ -9,11 +9,30 @@
 
 #include "BlockString.h"
 
+
+/* ---------- Constructors/Destructor ---------- */
+
+/*
+ * Instantiates a BlockString object using default values.
+ */
 BlockString::BlockString():
 Shape()
 {
 }
 
+/*
+ * Instantiates a BlockString object using the passed parameters.
+ * 
+ * Parameters:
+ *   int x: The value to initialize this BlockString object's x with
+ *   int y: The value to initialize this BlockString object's y with
+ *   int blockSize: The value to initialize this BlockString object's blockSize with
+ *   int padding: The value to initialize this BlockString object's padding with
+ *   string str: The value to initialize this BlockString object's string with, defaults to
+ *     the empty string
+ *   unsigned int background: The value to initialize this BlockString object's background
+ *     with, defaults to Color::BLACK
+ */
 BlockString::BlockString(int x, int y, int blockSize, int padding, string str,
         unsigned int background):
 Shape(x, y, blockSize, padding, background)
@@ -21,6 +40,15 @@ Shape(x, y, blockSize, padding, background)
     setString(str);
 }
 
+
+/* ---------- Public ---------- */
+
+/*
+ * Sets this BlockString object's blocks to represent the passed string.
+ * 
+ * Parameters:
+ *   string str: The string to model this BlockString after
+ */
 void BlockString::setString(string str) {
     
     for (unsigned int i = 0; i < blocks.size(); i++) {
--- src/BlockString.h
+++ src/BlockString.h
@@ -18,12 +18,36 @@
 using namespace std;
 
 class BlockString: public Shape {
+_registerForLeakcheckWithID(BlockString)
     public:
+        
+        /*
+         * Instantiates a BlockString object using default values.
+         */
         BlockString();
         
+        /*
+         * Instantiates a BlockString object using the passed parameters.
+         * 
+         * Parameters:
+         *   int x: The value to initialize this BlockString object's x with
+         *   int y: The value to initialize this BlockString object's y with
+         *   int blockSize: The value to initialize this BlockString object's blockSize with
+         *   int padding: The value to initialize this BlockString object's padding with
+         *   string str: The value to initialize this BlockString object's string with, defaults to
+         *     the empty string
+         *   unsigned int background: The value to initialize this BlockString object's background
+         *     with, defaults to Color::BLACK
+         */
         BlockString(int x, int y, int blockSize, int padding, string str = "",
                 unsigned int background = Color::BLACK);
         
+        /*
+         * Sets this BlockString object's blocks to represent the passed string.
+         * 
+         * Parameters:
+         *   string str: The string to model this BlockString after
+         */
         void setString(string str);
 };
 
